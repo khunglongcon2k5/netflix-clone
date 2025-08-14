5a5f8398db44e68b1d865c46e6e0fdc9b24e7311
Nguyễn Hùng <nguyentanhung413@gmail.com>
2025-08-14 23:44:57 +0700

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    modules: {
+      generateScopedName: mode === 'development'
+        ? '[name]__[local]___[hash:base64:3]'
+        : '[hash:base64:5]',
+
+      localsConvention: 'camelCaseOnly'
+    }
+  }
+}))
